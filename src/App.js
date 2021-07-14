0adbdbeee6dca441a68bb683d5ee0e7f70eb8431
thiagobastoscosta <thiagobastoscosta@gmail.com>
2021-07-14 13:01:52 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Ola Mundo!!!</h1>
+      <p>Muito importante viver o momento  :)</p>
+    </div>
+  );
+}
